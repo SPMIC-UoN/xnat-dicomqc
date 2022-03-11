c0d9ec0dbb8da777a5ada7c0e75a3805dd542615
Martin Craig <martin.craig@nottingham.ac.uk>
2022-03-11 17:01:49 +0000

    Work with session file structure and generate assessor XML

--- docker/dicom_qc.py
+++ docker/dicom_qc.py
@@ -64,7 +64,7 @@ def find_tag(ds, tag):
             if elem.tag == tag:
                 return elem
 
-def check_file(dcm, vendor):
+def check_file(dcm, vendor, qc_conf, scan_results):
     #print(dcm)
     print(f" - Series description: {dcm[(0x0008, 0x103e)].value}")
     matches = 0
@@ -84,12 +84,14 @@ def check_file(dcm, vendor):
                 print(f" - WARN: 'None' value for parameter: {conf.parameter}, tag: {key} in {fname}")
                 continue
             t = type(actual.value)
-            sys.stdout.write(f" - Checking: {conf.parameter} {actual.value} {conf.operator} {conf.expected}".ljust(70))
+            check = f"{conf.parameter} {actual.value} {conf.operator} {conf.expected}"
             if t in TYPE_HANDLERS:
                 if TYPE_HANDLERS[t](conf.parameter, actual.value, conf.operator, conf.expected):
-                    sys.stdout.write("PASS\n")
+                    print(check, "PASS")
+                    scan_results["passes"].add(check)
                 else:
-                    sys.stdout.write("FAIL\n")
+                    print(check, "FAIL")
+                    scan_results["fails"].add(check)
             else:
                 print(f" - WARN: No handler for {t} (parameter: {conf.parameter}, tag: {key}")
         if not found:
@@ -139,6 +141,7 @@ def get_vendor(dcm):
 
 TYPE_HANDLERS = {
     pydicom.valuerep.DSfloat : check_float,
+    pydicom.valuerep.IS : check_int,
     list : check_list,
     pydicom.multival.MultiValue : check_multival,
     int : check_int,
@@ -146,23 +149,93 @@ TYPE_HANDLERS = {
     str: check_str,
 }
 
-indir = sys.argv[1]
-#outdir = sys.argv[2]
-if len(sys.argv) > 2:
+def check_session(sessiondir, qc_conf):
+    scan_results = []
+    scansdir = [d for d in os.listdir(sessiondir) if d.lower() == "scans"]
+    if len(scansdir) != 1:
+        sys.stderr.write(f"ERROR: Expected single scan dir, got {scansdir}\n")
+        sys.exit(1)
+    scansdir = os.path.join(sessiondir, scansdir[0])
+
+    for scan in os.listdir(scansdir):
+        scan_checks = {"id" : scan, "passes" : set(), "fails" : set()}
+        scandir = os.path.join(scansdir, scan, "DICOM")
+        print(scandir, os.listdir(scandir))
+        for fname in os.listdir(scandir):
+            fpath = os.path.join(scandir, fname)
+            if not os.path.isfile(fpath):
+                continue
+            print(fname)
+            try:
+                with pydicom.dcmread(fpath) as dcm:
+                    vendor = get_vendor(dcm)
+                    if vendor not in qc_conf:
+                        print(f"WARN: Vendor {vendor} not found in QC config")
+                    else:
+                        print(f"Checking file: {fname} for vendor {vendor}")
+                        check_file(dcm, vendor, qc_conf, scan_checks)
+            except pydicom.errors.InvalidDicomError:
+                print(f"WARN: File {fname} for scan {scan} was not a DICOM file")
+        scan_results.append(scan_checks)
+    return scan_results
+
+XML_HEADER = """
+<DICOMQCData xmlns="http://github.com/spmic-uon/xnat-hpc" xmlns:xnat="http://nrg.wustl.edu/xnat" xsi:schemaLocation="http://github.com/spmic-uon/xnat-hpc schema.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <xnat:label>DICOMQC</xnat:label>
+  <dicomqcVersion>0.0.1</dicomqcVersion>
+"""
+
+XML_FOOTER = """
+</DICOMQCData>
+"""
+
+def make_xml(scan_results):
+    xml = XML_HEADER
+    overall_pass = not any([scan["fails"] for scan in scan_results])
+    if overall_pass:
+        xml += "  <overall_status>PASS</overall_status>\n"
+    else:
+        xml += "  <overall_status>FAIL</overall_status>\n"
+
+    for scan in scan_results:
+        xml += "  <scan>\n"
+        xml += f"    <scan_id>{scan['id']}</scan_id>\n"
+        for passed_test in scan["passes"]:
+            xml += f"    <passed_test>{passed_test}</passed_test>\n"
+        for failed_test in scan["fails"]:
+            xml += f"    <failed_test>{failed_test}</failed_test>\n"
+        xml += "  </scan>\n"
+    xml += XML_FOOTER
+    return xml
+
+def upload_xml(xml):
+    proj, subj, exp = sys.argv[3:]
+    #host = os.environ.get("XNAT_HOST").replace("http://", "https://") # hack
+    #os.environ["CURL_CA_BUNDLE"] = "" # FIXME Hack for cert validation disable
+    with open("temp.xml", "w") as f:
+        f.write(xml)
+    print('curl -u %s:%s -X POST "%s/data/projects/%s/subjects/%s/experiments/%s/assessors/" -F "file=@%s" -k' % (
+        os.environ["XNAT_USER"], os.environ["XNAT_PASS"], os.environ["XNAT_HOST"], 
+        proj, subj, exp, "temp.xml")
+    )
+
+def main():
+    if len(sys.argv) != 6:
+        sys.stderr.write("Usage: dicom_qc.py <indir> <conf_fname> <project id> <subject id> <session id>\n")
+    indir = sys.argv[1]
     config_fname = sys.argv[2]
-else:
-    config_fname = "dicomqc_conf.tsv"
-qc_conf = read_config(config_fname)
+    #os.system(f"find {indir}")
+    
+    qc_conf = read_config(config_fname)
+    sessions = os.listdir(indir)
+    if len(sessions) != 1:
+        sys.stderr.write(f"ERROR: Expected single session input, got {sessions}\n")
+        sys.exit(1)
+    sessiondir = os.path.join(indir, sessions[0])
 
-for fname in os.listdir(indir):
-    try:
-        with pydicom.dcmread(os.path.join(indir, fname)) as dcm:
-            vendor = get_vendor(dcm)
-            if vendor not in qc_conf:
-                print(f"WARN: Vendor {vendor} not found in QC config")
-            else:
-                print(f"Checking file: {fname} for vendor {vendor}")
-                check_file(dcm, vendor)
+    session_results = check_session(sessiondir, qc_conf)
+    xml = make_xml(session_results)
+    upload_xml(xml)
 
-    except pydicom.errors.InvalidDicomError:
-        print(f"WARN: File {fname} was not a DICOM file" % fname)
+if __name__ == "__main__":
+    main()
