9e15c2942723eb344d20dfe9b26128b58b0d0237
Martin Craig <martin.craig@nottingham.ac.uk>
2022-03-11 17:00:49 +0000

    Fix typo

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,7 @@ cp Dockerfile.in Dockerfile
 python scripts/cmd2label.py dicom_qc_cmd.json >> Dockerfile
 
 tag=0.0.1
-docker build -t martincraig/xnat-dicomqc.
+docker build -t martincraig/xnat-dicomqc .
 docker tag martincraig/xnat-dicomqc martincraig/xnat-dicomqc:$tag 
 docker push martincraig/xnat-dicomqc:$tag
 docker push martincraig/xnat-dicomqc:latest
