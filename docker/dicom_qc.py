a3c233d619dae745d137a5420e9a487888e98724
Martin Craig <martin.craig@nottingham.ac.uk>
2022-03-16 10:35:47 +0000

    Support OR and round floats to 'appropriate' decimal places Report warnings in XML output

--- docker/dicom_qc.py
+++ docker/dicom_qc.py
@@ -2,6 +2,9 @@ import os
 import sys
 import csv
 import types
+import datetime
+import requests
+import json
 
 import pydicom
 
@@ -18,7 +21,12 @@ def tag_from_text(txt):
 def check_float(parameter, value, operator, expected):
     value = float(value)
     if operator == "==":
-        return abs(value - float(expected)) < FLOAT_TOLERANCE
+        # Compare floats to the number of decimal places given in the expected value
+        if "." in expected:
+            num_dps = len(expected[expected.index(".")+1:].strip())
+        else:
+            num_dps = 0
+        return abs(round(value, num_dps) - round(float(expected), num_dps)) < FLOAT_TOLERANCE
     elif operator == "range":
         expected = [float(v) for v in expected.strip("[]").split(",")]
         return value >= expected[0] and value <= expected[1]
@@ -43,8 +51,7 @@ def check_list(parameter, value, operator, expected):
 def check_multival(parameter, value, operator, expected):
     print(" - checking multival")
 
-def series_matches(dcm, series_names):
-    series_desc = dcm[(0x0008, 0x103e)].value.lower()
+def series_matches(series_desc, series_names):
     for name in series_names:
         return name in series_desc
 
@@ -64,12 +71,32 @@ def find_tag(ds, tag):
             if elem.tag == tag:
                 return elem
 
+def check_value(parameter, actual, operator, expected, scan_results):
+    t = type(actual)
+    check = f"{parameter} {actual} {operator} {expected}"
+
+    if t in TYPE_HANDLERS:
+        if operator == "or":
+            possibles = [v for v in expected.strip("[]").split(",")]
+            if any([TYPE_HANDLERS[t](parameter, actual, "==", possible) for possible in possibles]):
+                print(check, "PASS")
+                scan_results["passes"].add(check)
+        elif TYPE_HANDLERS[t](parameter, actual, operator, expected):
+            print(check, "PASS")
+            scan_results["passes"].add(check)
+        else:
+            print(check, "FAIL")
+            scan_results["fails"].add(check)
+    else:
+        scan_results["warnings"].add(f"No handler for {t} (parameter: {conf.parameter}, tag: {key}")
+
 def check_file(dcm, vendor, qc_conf, scan_results):
     #print(dcm)
-    print(f" - Series description: {dcm[(0x0008, 0x103e)].value}")
+    series_desc = dcm[(0x0008, 0x103e)].value.lower()
+    print(f" - Series description: {series_desc}")
     matches = 0
     for conf in qc_conf[vendor]:
-        if not series_matches(dcm, conf.series_names):
+        if not series_matches(series_desc, conf.series_names):
             continue
         matches += 1
         found = False
@@ -81,23 +108,13 @@ def check_file(dcm, vendor, qc_conf, scan_results):
                 continue
             found = True
             if actual.value is None:
-                print(f" - WARN: 'None' value for parameter: {conf.parameter}, tag: {key} in {fname}")
+                scan_results["warnings"].add(f"'None' value for parameter: {conf.parameter}, tag: {key} in {fname}")
                 continue
-            t = type(actual.value)
-            check = f"{conf.parameter} {actual.value} {conf.operator} {conf.expected}"
-            if t in TYPE_HANDLERS:
-                if TYPE_HANDLERS[t](conf.parameter, actual.value, conf.operator, conf.expected):
-                    print(check, "PASS")
-                    scan_results["passes"].add(check)
-                else:
-                    print(check, "FAIL")
-                    scan_results["fails"].add(check)
-            else:
-                print(f" - WARN: No handler for {t} (parameter: {conf.parameter}, tag: {key}")
+            check_value(conf.parameter, actual.value, conf.operator, conf.expected, scan_results)
         if not found:
-            print(f" - WARN: No matching value to check for {conf.parameter}")
+            scan_results["warnings"].add(f"No matching value to check for {conf.parameter}")
     if matches == 0:
-        print(" - WARN: No matching checks for this series type")
+        scan_results["warnings"].add(f"No matching checks for series type {series_desc}")
 
 def read_config(fname):
     qc_conf = {}
@@ -151,15 +168,21 @@ TYPE_HANDLERS = {
 
 def check_session(sessiondir, qc_conf):
     scan_results = []
+    print("checking session")
     scansdir = [d for d in os.listdir(sessiondir) if d.lower() == "scans"]
+    print(scansdir)
     if len(scansdir) != 1:
         sys.stderr.write(f"ERROR: Expected single scan dir, got {scansdir}\n")
         sys.exit(1)
     scansdir = os.path.join(sessiondir, scansdir[0])
 
     for scan in os.listdir(scansdir):
-        scan_checks = {"id" : scan, "passes" : set(), "fails" : set()}
+        scan_checks = {"id" : scan, "passes" : set(), "fails" : set(), "warnings" : set()}
         scandir = os.path.join(scansdir, scan, "DICOM")
+        if not os.path.isdir(scandir):
+            print(f"WARN: No DICOMs for scan {scan}")
+            continue
+
         print(scandir, os.listdir(scandir))
         for fname in os.listdir(scandir):
             fpath = os.path.join(scandir, fname)
@@ -179,8 +202,8 @@ def check_session(sessiondir, qc_conf):
         scan_results.append(scan_checks)
     return scan_results
 
-XML_HEADER = """
-<DICOMQCData xmlns="http://github.com/spmic-uon/xnat-hpc" xmlns:xnat="http://nrg.wustl.edu/xnat" xsi:schemaLocation="http://github.com/spmic-uon/xnat-hpc schema.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+XML_HEADER = """<?xml version="1.0" encoding="UTF-8"?>
+<DICOMQCData xmlns="http://github.com/spmic-uon/xnat-dicomqc" xmlns:xnat="http://nrg.wustl.edu/xnat" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
   <xnat:label>DICOMQC</xnat:label>
   <dicomqcVersion>0.0.1</dicomqcVersion>
 """
@@ -191,6 +214,7 @@ XML_FOOTER = """
 
 def make_xml(scan_results):
     xml = XML_HEADER
+    xml += "  <xnat:date>%s</xnat:date>\n" % datetime.datetime.today().strftime('%Y-%m-%d')
     overall_pass = not any([scan["fails"] for scan in scan_results])
     if overall_pass:
         xml += "  <overall_status>PASS</overall_status>\n"
@@ -202,6 +226,8 @@ def make_xml(scan_results):
         xml += f"    <scan_id>{scan['id']}</scan_id>\n"
         for passed_test in scan["passes"]:
             xml += f"    <passed_test>{passed_test}</passed_test>\n"
+        for warning in scan["warnings"]:
+            xml += f"    <warning>{warning}</warning>\n"
         for failed_test in scan["fails"]:
             xml += f"    <failed_test>{failed_test}</failed_test>\n"
         xml += "  </scan>\n"
@@ -209,33 +235,56 @@ def make_xml(scan_results):
     return xml
 
 def upload_xml(xml):
+    print(os.environ["XNAT_HOST"], os.environ["XNAT_USER"], os.environ["XNAT_PASS"])
     proj, subj, exp = sys.argv[3:]
-    #host = os.environ.get("XNAT_HOST").replace("http://", "https://") # hack
-    #os.environ["CURL_CA_BUNDLE"] = "" # FIXME Hack for cert validation disable
+    host = os.environ.get("XNAT_HOST").replace("http://", "https://") # FIXME hack
+    os.environ["CURL_CA_BUNDLE"] = "" # FIXME Hack for cert validation disable
     with open("temp.xml", "w") as f:
         f.write(xml)
-    print('curl -u %s:%s -X POST "%s/data/projects/%s/subjects/%s/experiments/%s/assessors/" -F "file=@%s" -k' % (
-        os.environ["XNAT_USER"], os.environ["XNAT_PASS"], os.environ["XNAT_HOST"], 
-        proj, subj, exp, "temp.xml")
-    )
+        
+    print(xml)
+    with open("temp.xml", "r") as f:
+        files = {'file': f}
+        #headers = {'Content-Type': 'application/xml'} # set what your server accepts
+        #params = {'xsiType': 'spmic:DICOMQCData'}
+        r = requests.post("%s/data/projects/%s/subjects/%s/experiments/%s/assessors/" % (host, proj, subj, exp), files=files, auth=(os.environ["XNAT_USER"], os.environ["XNAT_PASS"]))
+        print(r.text)
+    #os.environ["XNAT_HOST"], auth=(os.environ["XNAT_USER"], os.environ["XNAT_PASS"]))
+    #upload_cmd = 'xnatc --user="%s" --password="%s" --xnat="%s" --project="%s" --subject="%s" --experiment="%s" --create-assessor="%s" --debug' % (
+    #    user, passwd, host, 
+    #    proj, subj, exp, "temp.xml")
+    #print(upload_cmd)
+    #os.system(upload_cmd)
+
+def get_alias():
+    r = requests.get("%s/data/services/tokens/issue" % os.environ["XNAT_HOST"], auth=(os.environ["XNAT_USER"], os.environ["XNAT_PASS"]))
+    if r.status_code != 200:
+        print("Error getting alias: ", r.text)
+        sys.exit(1)
+    else:
+        result = json.loads(r.text)
+        print(result)
+        return result["alias"], result["secret"]
 
 def main():
     if len(sys.argv) != 6:
         sys.stderr.write("Usage: dicom_qc.py <indir> <conf_fname> <project id> <subject id> <session id>\n")
     indir = sys.argv[1]
     config_fname = sys.argv[2]
-    #os.system(f"find {indir}")
-    
     qc_conf = read_config(config_fname)
-    sessions = os.listdir(indir)
-    if len(sessions) != 1:
-        sys.stderr.write(f"ERROR: Expected single session input, got {sessions}\n")
-        sys.exit(1)
-    sessiondir = os.path.join(indir, sessions[0])
+    #user, passwd = get_alias()
 
-    session_results = check_session(sessiondir, qc_conf)
-    xml = make_xml(session_results)
-    upload_xml(xml)
+    found_session = False
+    for path, dirs, files in os.walk(indir):
+        if "scans" in [d.lower() for d in dirs]:
+            if not found_session:
+                print(f"Found session with scans in {path}")
+                found_session = True
+                session_results = check_session(path, qc_conf)
+                xml = make_xml(session_results)
+                upload_xml(xml)
+            else:
+                print("WARN: Found another session: {path} - ignoring")
 
 if __name__ == "__main__":
     main()
