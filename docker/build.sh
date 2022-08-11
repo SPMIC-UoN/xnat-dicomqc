8b53ce43cf71e4551dfdd02b643c73efd75b1845
Martin Craig <martin.craig@nottingham.ac.uk>
2022-08-11 15:48:12 +0100

    Bump container version

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,7 @@
 cp Dockerfile.in Dockerfile
 python scripts/cmd2label.py dicom_qc_cmd.json >> Dockerfile
 
-tag=0.0.1
+tag=0.0.2
 docker build -t martincraig/xnat-dicomqc .
 docker tag martincraig/xnat-dicomqc martincraig/xnat-dicomqc:$tag 
 docker push martincraig/xnat-dicomqc:$tag
