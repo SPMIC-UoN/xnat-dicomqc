2877755f8270b0b7d9b6f918c657519b9810ab44
Martin Craig <martin.craig@nottingham.ac.uk>
2022-10-12 14:43:52 +0100

    Separate versioning file

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,7 @@ cp Dockerfile.in Dockerfile
 python scripts/cmd2label.py dicom_qc_cmd.json >> Dockerfile
 cp ../config/ukrin_conf.xlsx .
 
-tag=0.0.2
+tag=`cat version.txt`
 docker build -t martincraig/xnat-dicomqc .
 docker tag martincraig/xnat-dicomqc martincraig/xnat-dicomqc:$tag 
 docker push martincraig/xnat-dicomqc:$tag
